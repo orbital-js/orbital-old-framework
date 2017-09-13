5c8c0471b30e06c1b398c1e0da63fb258ddb61a5
Wilson Hobbs <wilsonhobbs1@gmail.com>
2017-09-12 22:38:22 -0400

    fix(mongo): export ObjectID class in package

--- packages/mongo/src/index.ts
+++ packages/mongo/src/index.ts
@@ -1,6 +1,7 @@
-/** @module @orbital/http */
+/** @module @orbital/mongo */
 
-export { MongoModule } from './module';
 export { Mongo } from './mongo';
 export { MongoClientConfig } from './mongo_client_config';
 export { MongoConfig } from './mongo_config';
+export { MongoModule } from './module';
+export { ObjectID } from 'mongodb';
